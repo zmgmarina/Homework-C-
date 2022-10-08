6963a1228bac7b132f8c839cdd347c4e5bcde779
Marina <zmgmarina@mail.ru>
2022-10-09 01:36:42 +0500

    Add Lesson 007

---
+++ Lesson_007/Task47/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5  7  -2  -0,2
+// 1  -3,3  8  -9,9
+// 8  7,8  -7,1  9
+
+int rows = Convert.ToDouble(Console.ReadLine());
+int colums = Convert.ToDouble(Console.ReadLine());
+
+
+// Заполнение массива рандомными вещественными числами
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+//  Функция вывода массива в терминал
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+PrintArray2D(FillArray2D);
